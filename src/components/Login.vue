ad4a63014f8ff4dc59207215975d1d323faa9910
Aryeon <41093670+winkpsj0529@users.noreply.github.com>
2019-12-04 07:49:45 +0900

    [11]Upload_Login

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,91 @@
 <template>
-  <div>
-    <h1>Login</h1>
-  </div>
+  <v-app id="inspire">
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <v-row
+          align="center"
+          justify="center"
+        >
+          <v-col
+            cols="12"
+            sm="8"
+            md="4"
+          >
+            <v-card class="elevation-12">
+              <v-toolbar
+                color="pink lighten-3"
+                dark
+                flat
+              >
+                
+                <v-toolbar-title>Bow-Wow Login</v-toolbar-title>
+                <v-spacer />
+                <v-tooltip bottom>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      :href="source"
+                      icon
+                      large
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-code-tags</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Source</span>
+                </v-tooltip>
+                <v-tooltip right>
+                  <template v-slot:activator="{ on }">
+                    <v-btn
+                      icon
+                      large
+                      href="https://codepen.io/johnjleider/pen/pMvGQO"
+                      target="_blank"
+                      v-on="on"
+                    >
+                      <v-icon>mdi-codepen</v-icon>
+                    </v-btn>
+                  </template>
+                  <span>Codepen</span>
+                </v-tooltip>
+              </v-toolbar>
+              <v-card-text>
+                <v-form>
+                  <v-text-field
+                    label="ID"
+                    name="id"
+                    type="text"
+                  />
+
+                  <v-text-field
+                    id="password"
+                    label="Password"
+                    name="password"
+                    type="password"
+                  />
+                </v-form>
+              </v-card-text>
+              <v-card-actions>
+                <v-spacer />
+                <v-btn text large color="pink lighten-1">Login</v-btn>
+                <v-btn text large color="pink lighten-1">Sign-in</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style scoped>
-h1 {
-  color: green
-}
-</style>
-
 <script>
+export default {
+  name: "Login",
+  props: {
+    source:String,
+  }
+};
 </script>
