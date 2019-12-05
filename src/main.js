8be06de75468324e4ef92b271c63d0ed064a3021
Unperknown <wnsdud105@gmail.com>
2019-12-05 20:24:16 +0900

    [18] Added : Express Backend

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import axios from 'axios'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -8,6 +9,7 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
+Vue.prototype.$http = axios
 
 new Vue({
   router,
