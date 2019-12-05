e4b0eb1271de9be6d8bbf6a60a8b5c9864e8c445
Unperknown <wnsdud105@gmail.com>
2019-12-05 21:12:42 +0900

    [19] Fixed : npm Packages

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import axios from 'axios'
+
 import App from './App.vue'
-import './registerServiceWorker'
+
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
+
+import './registerServiceWorker'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios
