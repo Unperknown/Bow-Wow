9c656e8b43141f374a337d52e3a28ffc7673297c
Unperknown <wnsdud105@gmail.com>
2019-12-05 14:15:15 +0900

    [17] Updated : Service Worker Config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   'transpileDependencies': [
     'vuetify'
-  ]
+  ],
+  pwa: {
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'public/service-worker.js',
+    }
+  }
 }
