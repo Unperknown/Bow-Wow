f71683118448eeb64aed652dbfef6ee43ea313e1
Aryeon <41093670+winkpsj0529@users.noreply.github.com>
2019-12-07 18:37:31 +0900

    [vue-5] Upload: SplashScreen.vue, Clinic.vue, PetCare.vue

---
+++ src/components/PetCare.vue
@@ -0,0 +1,126 @@
+<template>
+<v-app id="app">
+<v-card
+    color="grey lighten-4"
+    flat
+    height="50px"
+    tile
+    mb-0
+  >
+    <v-toolbar dense flat>
+      <v-app-bar-nav-icon>
+        <v-img src="@/assets/logo(2).png" width="30px">
+        </v-img>
+      </v-app-bar-nav-icon>
+      <v-toolbar-title>Bow-Wow</v-toolbar-title>
+      <v-row justify="end">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      </v-row>
+    </v-toolbar>
+  </v-card>
+<v-comtainer>
+  <v-row mt-5>
+      <v-col order="first">
+        <v-hover v-slot:default="{ hover }">
+          <v-card
+          class="pa-2 mx-auto"
+          :elevation="hover ? 12 : 2"
+          height="200"
+          max-width="200"
+        >
+          <v-row justify="center">
+          <v-icon x-large light>
+            apartment
+          </v-icon>
+          </v-row>
+          <v-card-text class="my-4 text-center title">병원찾기</v-card-text>
+        </v-card>
+        </v-hover>
+      </v-col>
+      <v-col>
+        <v-hover v-slot:default="{ hover }">
+          <v-card
+          class="pa-2 mx-auto"
+          :elevation="hover ? 12 : 2"
+          height="200"
+          max-width="200"
+        >
+          <v-row justify="center">
+          <v-icon x-large light>
+            date_range
+          </v-icon>
+          </v-row>
+          <v-card-text class="my-4 text-center title">진료예약</v-card-text>
+        </v-card>
+        </v-hover>
+      </v-col>
+      <v-col order="last">
+        <v-hover v-slot:default="{ hover }">
+          <v-card
+          class="pa-2 mx-auto"
+          :elevation="hover ? 12 : 2"
+          height="200"
+          max-width="200"
+        >
+        <v-row justify="center">
+          <v-icon x-large light>
+            access_alarms
+          </v-icon>
+        </v-row>
+          <v-card-text class="my-4 text-center title">먹이알림</v-card-text>
+        </v-card>
+        </v-hover>
+      </v-col>
+    </v-row>
+  <v-row
+    align="center"
+    justify="center">
+    <v-col
+      xs="12"
+      sm="7"
+      md="6"
+      lg="5"
+      xl2="2"
+    >
+     <v-bottom-navigation fixed="true"
+      :value="activeBtn"
+      grow
+      color="pink lighten-1"
+       >
+        <v-btn>
+          <span>실종</span>
+          <v-icon>scatter_plot</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>SNS</span>
+          <v-icon>mode_comment</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>동물관리</span>
+          <v-icon>healing</v-icon>
+        </v-btn>
+
+          <v-btn>
+            <span>추천</span>
+            <v-icon>thumb_up_alt</v-icon>
+          </v-btn>
+
+        </v-bottom-navigation>
+      </v-col>
+   </v-row>
+  </v-comtainer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'PetCare',
+  data () {
+    return {
+      bottomNav: 'recent'
+    }
+  }
+}
+</script>
--- src/components/SplashScreen.vue
+++ src/components/SplashScreen.vue
@@ -21,6 +21,6 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'SplashScreen'
 }
 </script>
