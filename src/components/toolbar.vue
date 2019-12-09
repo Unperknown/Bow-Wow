77ba612b01b34108e119aa1938794da9a6cc1537
Aryeon <41093670+winkpsj0529@users.noreply.github.com>
2019-12-09 14:14:24 +0900

    [v-9] Changed_toolbar.vue

--- src/components/toolbar.vue
+++ src/components/toolbar.vue
@@ -7,14 +7,22 @@
     tile
     mb-0
   >
-    <v-toolbar dense flat>
+        <v-toolbar dense flat>
       <v-app-bar-nav-icon>
         <v-img src="@/assets/logo(2).png" width="30px">
         </v-img>
       </v-app-bar-nav-icon>
       <v-toolbar-title>Bow-Wow</v-toolbar-title>
-      <v-row justify="end">
-        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-row align="center" justify="end">
+      <span class="caption">OOO님, 환영합니다.</span>
+      <v-avatar>
+         <img src="https://randomuser.me/api/portraits/women/81.jpg">
+      </v-avatar>
+        <v-btn color="grey"
+    icon>
+    <v-img src="@/assets/logout.png">
+    </v-img>
+  </v-btn>
       </v-row>
     </v-toolbar>
   </v-card>
