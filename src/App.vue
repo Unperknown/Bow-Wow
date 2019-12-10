2ab0e85a0c6a6daa999d3265a22a39e6667c022e
Aryeon <41093670+winkpsj0529@users.noreply.github.com>
2019-12-10 14:44:11 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,94 @@
 <template>
-    <v-app>
-        <v-card
+<v-app id="app">
+<v-card
     color="grey lighten-4"
     flat
     height="50px"
     tile
     mb-0
   >
-        <v-toolbar dense flat>
+    <v-toolbar dense flat>
       <v-app-bar-nav-icon>
         <v-img src="@/assets/logo(2).png" width="30px">
         </v-img>
       </v-app-bar-nav-icon>
       <v-toolbar-title>Bow-Wow</v-toolbar-title>
-      <v-row align="center" justify="end">
-      <span class="caption">OOO님, 환영합니다.</span>
-      <v-avatar>
-         <img src="https://randomuser.me/api/portraits/women/81.jpg">
-      </v-avatar>
-        <v-btn color="grey"
-    icon>
-    <v-img src="@/assets/logout.png">
-    </v-img>
-  </v-btn>
+      <v-row justify="end">
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
       </v-row>
     </v-toolbar>
   </v-card>
-    </v-app>
+<v-comtainer>
+  <v-row
+    align="center"
+    justify="center">
+    <v-col
+      xs="12"
+      sm="7"
+      md="6"
+      lg="5"
+      xl2="2"
+    >
+      <v-file-input
+      multiple
+    accept="image/png, image/jpeg, image/bmp"
+    placeholder="Put up a picture of your pet."
+    prepend-icon="camera"
+    label="Picture"
+  ></v-file-input>
+      <v-card
+        flat class="mx-auto">
+        <v-card-text>
+      <div>Writing</div>
+      <v-textarea
+          flat
+          solo
+          name="input-7-4"
+          label="text"
+        ></v-textarea>
+        <v-row justify="end">
+          <v-btn text large color="pink lighten-1">Submit</v-btn>
+        </v-row>
+    </v-card-text>
+      </v-card>
+      <v-bottom-navigation fixed="true"
+      :value="activeBtn"
+      grow
+      color="pink lighten-1"
+       >
+        <v-btn>
+          <span>실종</span>
+          <v-icon>scatter_plot</v-icon>
+        </v-btn>
+
+        <v-btn>
+          <span>SNS</span>
+          <v-icon>mode_comment</v-icon>
+        </v-btn>
+        <v-btn>
+          <span>동물관리</span>
+          <v-icon>healing</v-icon>
+        </v-btn>
+
+          <v-btn>
+            <span>추천</span>
+            <v-icon>thumb_up_alt</v-icon>
+          </v-btn>
+
+        </v-bottom-navigation>
+      </v-col>
+   </v-row>
+  </v-comtainer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'Writing',
+  data () {
+    return {
+      bottomNav: 'recent'
+    }
+  }
 }
 </script>
